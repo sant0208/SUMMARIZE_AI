f3db9ee0491a8b311824a30e683228ed172d8d15
SANTHOSH <147644429+sant0208@users.noreply.github.com>
2024-04-20 01:03:28 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
       </nav>
 
       <h1 className='head_text'>
-      Summarize Articles with <br className='max-md:hidden' />
+      Summa Articles with <br className='max-md:hidden' />
         <span className='orange_gradient '>OpenAI GPT-4</span>
       </h1>
       <h2 className='desc'>
